526f5245cd3a0f23c3810d2015af63652711ea19
Bohdan Marchenko <kamimotoptop@gmail.com>
2024-08-03 19:30:48 +0200

    sdasd

--- Assets/Scripts/bUTTON.cs
+++ Assets/Scripts/bUTTON.cs
@@ -1,16 +1,21 @@
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
-public class bUTTON : MonoBehaviour
+public class Button : MonoBehaviour
 {
-    public void GoToScene(string sceneName)
+    // Method to load the next scene in the build index
+    public void PlayGame()
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // Method to quit the application
     public void QuitApp()
     {
         Application.Quit();
-        Debug.Log("poka negr");
+        Debug.Log("Application has been quit.");
     }
 }
+
