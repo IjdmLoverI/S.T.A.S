4b58b4bf45d887ed02e7ec4fbed3701445909220
Kyrylo <kbuchinskiy1@gmail.com>
2024-08-09 15:42:05 +0300

    add test enemy patrolling

---
+++ Assets/Scripts/PatrolEnemy.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolEnemy : MonoBehaviour
+{
+
+    public float speed;
+    public int positionOfPatrol;
+    public Transform point;
+    public bool movingRight;
+    public Transform player;
+    public float stoppingDistance;
+
+    public bool chill = false;
+    public bool angry = false;
+    public bool goBack = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, point.position) < positionOfPatrol && angry == false)
+        {
+            chill = true;
+        }
+
+        if (Vector2.Distance(transform.position, player.position) < stoppingDistance)
+        {
+            angry = true;
+            chill = false;
+            goBack = false;
+        }
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            goBack = true;
+            angry = false;
+        }
+        
+        if (chill == true)
+        {
+            Chill();
+        }
+        else if (angry == true)
+        {
+            Angry();
+        }
+        else if (goBack == true)
+        {
+            GoBack();
+        }
+        
+    }
+
+    void Chill()
+    {
+        if (transform.position.x > point.position.x + positionOfPatrol)
+        {
+            movingRight = false;
+        }
+        else if (transform.position.x < point.position.x - positionOfPatrol)
+        {
+            movingRight = true;
+        }
+
+        if (movingRight)
+        {
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        }
+    }
+
+    void Angry()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+
+    void GoBack()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, point.position, speed * Time.deltaTime);
+    }
+}
